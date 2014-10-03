352d886f0da736d1fc0b4d4a91226fa24d0b3375
Castro0o <andre@andrecastro.info>
2014-10-03 14:09:04 +0200

    replacing files(img) src to URL

--- mediawiki.py
+++ mediawiki.py
@@ -14,17 +14,20 @@ useragent = "Mozilla/5.001 (windows; U; NT4.0; en-US; rv:1.0) Gecko/25250101"
 endpoint = "http://localhost/wiki/api.php?format=json&"
 #endpoint = "http://en.wikipedia.org/w/api.php?format=json&"
 
+
+
 #query='action=query&list=categorymembers&cmtitle=Category:project'
 #http://localhost/wiki/api.php?format=json&action=query&list=categorymembers&cmtitle=Category:project
 
 html_template = """
+<!DOCTYPE html>
 <html>
 <head>
 <title>{{ title }}</title>
 <style type="text/css" media="screen">
   body{background:#cacc00;
   font-family: Sans;
-    }
+   }
     
   a{color:#b213ab;}
 
@@ -56,7 +59,7 @@ def pandoc(mw_content):
     mw = open('content.mw', 'w') 
     mw.write(mw_content.encode('utf-8'))
     mw.close()
-    pandoc = 'pandoc -f mediawiki -t html5  content.mw -o content.html'
+    pandoc = 'pandoc -f mediawiki -t html5 content.mw -o content.html'
     subprocess.call(pandoc, shell=True) # saved in content.html html
     html = open('content.html', 'r') #write mediawiki content to content.mw
     html = html.read()
@@ -81,8 +84,7 @@ def api_page(pagename, info):
 
 def api_page_content(pagename):
     page = api_page(pagename, 'content')
-    print 'PAGE', page
-    # get title
+    print 'PAGE', pagename
     content = ((page.get('revisions'))[0])['*']
     return content
 #    print json.dumps( revisions, sort_keys=True, indent=4) ## see response
@@ -90,17 +92,18 @@ def api_page_content(pagename):
 
 def api_page_protection(pagename):
     page = api_page(pagename, 'protection')
-    print 'Keys', page.keys()
+#    print 'Keys', page.keys()
     page_protection = page.get('protection')
-    print 'page_protection', page_protection
     if page_protection: # page is protected
         print "PROTECTED PAGE"
-        api_page_content(pagename)
+        return api_page_content(pagename)
     else:       
         print 'PAGE NOT PROTECTED :('
+#        api_page_content(pagename, page_title)
 
 
-def api_pagesInCategory( category):
+########### TO DO
+def api_pagesInCategory( category): 
     query = endpoint + 'action=query&list=categorymembers&cmtitle=Category:{}'.format(category)
     print query
     url = endpoint + query
@@ -109,20 +112,57 @@ def api_pagesInCategory( category):
     for item in  jsonp['query']['categorymembers']:
         print item['title']
         print json.dumps(item, sort_keys=True, indent=4)
-
 #api_pagesInCategory('project')
+#########
+
+from xml.etree import ElementTree as ET
+
+
+def api_file_url(filename):
+    query = endpoint + "action=query&titles=File:{}&prop=imageinfo&iiprop=url".format(filename)
+    url = endpoint + query
+    request = urllib2.urlopen(url)
+    jsonp = json.loads(request.read())    
+    json_dic= (jsonp.get('query').get('pages'))
+    page_id =  json_dic.keys()[0]
+    page_url = json_dic.get(page_id)
+    page_url = (page_url['imageinfo'][0]).get('url')
+    return page_url 
+
+def parse_html(): #(pagename)
+    htmlpage = 'page_wiki.html'
+    tree = ET.parse(htmlpage)
+    root = tree.getroot()
+
+    for img in tree.findall('.//img'):
+        src = img.get('src')
+        url = api_file_url(src)# find url of file
+        img.set('src', url)
+        print ET.tostring(img)
+    
+    # embed: audio & video
+    for embed in tree.findall('.//embed'):
+        src = embed.get('src')
+        print ET.tostring(embed), 'SRC:', src
+        # FIND URL OF FILE!
+        url = api_file_url(src)
+
+    # WRITE FILE
+
 
 
 
 mw_page = sys.argv[1]
-content_mw = api_page_protection(mw_page) #only retreive content if page is protected
-# if not interested in Protection use
-#content_mw=api_page_content(mw_page) 
+content_mw = api_page_protection(mw_page) #retreive protected page mw content
+# IF NOT INTERESTED IN PROTECTED PAGES, USE: 
+# content_mw = api_page_content(mw_page) 
 
 if content_mw:    
     content_html = pandoc(content_mw)
+    full_html = template(mw_page, content_html) 
     # TODO: parse content_html 
     # TO: replace relative file paths with  full url
-    full_html = template(mw_page, content_html) 
-    print full_html
+    parse_html() #(page name)
+
+#    print full_html
 
