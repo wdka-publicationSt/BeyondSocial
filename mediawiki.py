55427a4db485468be719bfa16439ef7c5c85bb94
Castro0o <andre@andrecastro.info>
2014-11-09 12:54:00 +0100

    draft index

--- mediawiki.py
+++ mediawiki.py
@@ -3,26 +3,17 @@
 
 ##############
 # TO DO
-# * finish  def edit_index - so that index is created and new articles addedd
+# bulding index
+# 
+#
+# place index.html.bak in git
+
+
+# wiki_2_html(article)  # TO BE DEALT WITH 
 #
 # Bugs 
 # iframe creation in youtube tag
-'''
-<Element 'iframe' at 0x7f7db6fd6710> <iframe width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen" src="http://www.youtube.com/embed/hIR3VW5DQ_o" /> <Element u'p' at 0x7f7db701a240>
-Traceback (most recent call last):
-  File "./mediawiki.py", line 264, in <module>
-    edit_index(articles_issue_dic, 'issue0_index.html')
-  File "./mediawiki.py", line 203, in edit_index
-    wiki_2_html(article) 
-  File "./mediawiki.py", line 130, in wiki_2_html
-    edit_html_media(full_html , endpoint, html_file)
-  File "/home/andre/Documents/WdKA/BeyondSocial/wiki_api/edit_html.py", line 45, in edit_html_media
-    p.append(embed_element)
-TypeError: must be Element, not Element
-
-'''
-###
-
+#####
 
 #
 import xml.etree.ElementTree as ET
@@ -38,15 +29,16 @@ from edit_html import edit_html_media
 
 sid = '1234'
 useragent = "Mozilla/5.001 (windows; U; NT4.0; en-US; rv:1.0) Gecko/25250101"
-endpoint = "http://localhost/wiki/api.php?format=json&"
-#endpoint = "http://en.wikipedia.org/w/api.php?format=json&"
+endpoint = "http://beyond-social.org/wiki/api.php?format=json&"
 
-#query='action=query&list=categorymembers&cmtitle=Category:project'
-#http://localhost/wiki/api.php?format=json&action=query&list=categorymembers&cmtitle=Category:project
+category_topic = ['Aesthetics', 'Bottom-up', 'Economics', 'Failures', 'Participation', 'Politics', 'Strategies', 'Transformation', 'Visions']
+category_section = ['Discourses', 'Introduction', 'Projects', 'Proposals' ]
+category_state = [ '01 Write Me', '02 Edit Me', '03 Proof Me', '04 Publish Me']
+
 
 
 def write_html_file(html_content, filename):
-    doctype = "<!DOCTYPE html>"
+    doctype = "<!DOCTYPE HTML>"
     html = doctype + html_content #ET.tostring(tree,  encoding='utf-8', method='html')
     edited = open(filename, 'w') #write
     edited.write(html)
@@ -72,6 +64,15 @@ article_template = """
 <head>
 <title>{{ title }}</title>
 <meta charset="utf-8" />
+<style type="text/css" media="screen">
+  body{background:#cacc00;
+  font-family: Sans;
+   }
+    
+  a{color:#b213ab;}
+
+  a:visited{color:#b213ab;}
+</style>
 </head>
 <body>
 <h1>{{ title }}</h1>
@@ -96,42 +97,23 @@ def pandoc(mw_content):
     return html
  
 
-def prepare_response(api, info): 
-    '''prepares responses from api'''
-    request = urllib2.urlopen(api)
-    jsonp = json.loads(request.read() )
-    json_dict= (jsonp.get('query').get('pages'))
-    page_id =  json_dict.keys()[0]
-    json_dict = json_dict.get(page_id)
-    if info in ['categories'] : #if info=categories Output: category_list. 
-        #categories: ['BeyondSocial', 'Issue0', 'Test']
-        category_list = json_dict.get('categories')
-        category_list = [  ((category['title'].encode('utf-8')).replace('Category:', '')).replace(' ','_') for category in category_list]
-        response = category_list
-    else:  #if info=content|meta Output: json_dict
-        response = json_dict 
-    return response
-
 
 def api_page(pagename, info):
-    '''ALL THE INFO ON AN ARTICLE: content, meta, categories, ''' 
-
     if info == 'content':        
-        url = endpoint + 'action=query&titles={}&prop=revisions&rvprop=content'.format(pagename) 
-        print 'CONTENT', url
+        url = endpoint + 'action=query&titles={}&prop=revisions&rvprop=content'.format(pagename) #no protection info in this url
     elif info == 'meta':
         url = endpoint + 'action=query&titles={}&prop=info&inprop=protection'.format(pagename)
-        print 'META', url
+        print 'META URL', url
+    print url
+    request = urllib2.urlopen(url)
+    jsonp = json.loads(request.read() )
+    json_dic= (jsonp.get('query').get('pages'))
+    page_id =  json_dic.keys()[0]
+    page_content = json_dic.get(page_id)
+    return page_content
 
-    elif info == 'categories':
-        url = endpoint + 'action=query&titles={}&prop=categories'.format(pagename)
-        print 'CATEGORIES', url
 
-    response = prepare_response(url, info)
-    return response
-
-
-def api_page_content(pagename): # MOVE THIS def INSIDE prepare_response ??
+def api_page_content(pagename):
     page = api_page(pagename, 'content')
     print 'PAGE', pagename
     content = ((page.get('revisions'))[0])['*']
@@ -139,8 +121,7 @@ def api_page_content(pagename): # MOVE THIS def INSIDE prepare_response ??
 #    print json.dumps( revisions, sort_keys=True, indent=4) ## see response
     
 
-def api_page_protection(pagename):  #IS THIS NEEDED? CAN'T ONLY api_page_content BE USED?
-    # MOVE THIS def INSIDE prepare_response ??
+def api_page_protection(pagename):
     page = api_page(pagename, 'meta')
 #    print 'Keys', page.keys()
     page_protection = page.get('protection')
@@ -163,102 +144,174 @@ def wiki_2_html(mw_page):
         content_html = pandoc(content_mw)
         full_html = template(mw_page, content_html, article_template) 
         edit_html_media(full_html , endpoint, html_file)
+
     #    print full_html
+
+#wiki_2_html(sys.argv[1]) 
+
 
 
 
 
 ################### index #######################
 
-def api_pagesInIssue(issue): 
-    '''Finds all pages within IssueN category
-    Returns a DICTIONARY with metadata(page id, touched date) and category list of those pages,
-    if they protected.    
-    '''    
-    dict_pagesInIssue = {}
-    query = endpoint + 'action=query&list=categorymembers&cmtitle=Category:{}'.format(issue)
+
+def api_categoriesFromPage(page):
+    '''Find all the categories, and their parent category of a page '''
+    topic = []
+    query = 'action=query&titles={}&prop=categories'.format(page)
+    url = endpoint + query
+    request = urllib2.urlopen(url)
+#    print 'categories',url
+    jsonp = json.loads(request.read())    
+    json_dic = jsonp['query']['pages']
+    page_id =  json_dic.keys()[0]
+    page_categories = json_dic[page_id][u'categories']
+    for entry in page_categories:
+        category =  (entry[u'title'].encode('utf-8')).replace('Category:', '')
+        if category in category_topic:
+            topic.append(category)
+        elif category in category_section:
+            section = category
+    return (topic, section)
+
+
+
+def api_pagesInCategory(category): 
+    '''Finds all pages within Issue N category and return a dictionary with info on those pages'''
+    dic_categ = {}
+    query = endpoint + 'action=query&list=categorymembers&cmtitle=Category:{}&cmtitle=Category:04_Publish_Me'.format(category)
     print 'Category: ', query
     url = endpoint + query
     request = urllib2.urlopen(url)
     jsonp = json.loads(request.read())    
-    print 'JSONP', jsonp['query']['categorymembers']
+    print 'CATEGORY MEMBERS', jsonp['query']['categorymembers']
 
-    for item in  jsonp['query']['categorymembers']:      
-        article_meta = api_page(item['title'], 'meta')
-        article_categories = api_page(item['title'], 'categories')
+    for page in  jsonp['query']['categorymembers']:      
+        page['title'] = (page['title']).replace(" ", "_") #use snakecase for page titles
+        print 'page', page
+        article_meta = api_page(page['title'], 'meta')
+        print 'article_meta', article_meta
+        category_topic = api_categoriesFromPage(page['title'])[0]
+        category_section = api_categoriesFromPage(page['title'])[1]
 
-        if len(article_meta['protection']) > 0:
-            dict_pagesInIssue[ str(item['title']) ] = { 'pageid': article_meta['pageid'], 
-                                           'touched': str(article_meta['touched'])[:-1],
-                                                        'categories': article_categories,
-                                           } 
-    return dict_pagesInIssue
+        dic_categ[ str(page['title']) ] = { 'pageid': article_meta['pageid'], 
+                                            'touched': str(article_meta['touched'])[:-1],
+                                            'topic': category_topic,
+                                            'section': category_section,
+                                            'issue': category, #category = issue
+                                            } 
+    return dic_categ 
 
 
-def index_article_add(ul):
-    '''adds <li> containing new articles to index <ul>'''
-    child_li = ET.SubElement(ul, 'li') # added news
-    grandchild_a = ET.SubElement(child_li, 'a') # added new
-    return child_li, grandchild_a
 
-def index_article_set(article_name, child_li, grandchild_a, dictionary):
-    '''sets index's <li> with atrributes and values'''
-    child_li.set('data-name', article_name )
-    child_li.set('data-touched', dictionary[article_name]['touched'])            
-    grandchild_a.text = article_name
-    grandchild_a.set('href', 'html_articles/'+((article_name.split('/'))[-1])+'.html' )
-    grandchild_a.set('data-name', article_name )
-    categories = ' '.join(dictionary[article_name]['categories'])
-    child_li.set('data-categories', categories )
-    wiki_2_html(article_name) # CREATE article's to content file with def wiki_2_html
+
+
+
+
 
 
 def edit_index( articles_dict, index_path ): 
     ''' Compares articles_dictionary with the index file 
     if there are new articles in mediawiki:
     def adds them to index file, and triggers the creation of the content file for that article (via wiki_2_html def)'''
+
     index_file = open(index_path, 'r') 
     index_tree = html5lib.parse(index_file, namespaceHTMLElements=False)
-    index_ul = index_tree.findall('.//ul')[0]
+    index_ul = index_tree.findall('.//ul[@class="list"]')[0]
+    print 'index_ul', index_ul, #ET.tostring(index_ul)
+
     index_items = index_tree.findall('.//li')
     index_items_data_name = [ (li.get('data-name')).encode('utf-8') for li in index_items]
     index_items_data_touched = [ (li.get('data-touched')).encode('utf-8') for li in index_items]
+
     for article in articles_dict.keys():  # compare the api results to the contents of index.html
-        if article not in index_items_data_name: #if new
-#            print 'NEW ARTICLE', article
-            li_anchor = index_article_add(index_ul) # add
-            index_article_set(article, li_anchor[0], li_anchor[1], articles_dict) # set
-            wiki_2_html(article) # CREATE article's to content file with def wiki_2_html
-        else: # if already in index
+        print "***********"
+        print "ARTICLE", article
+        print "***********"
+
+        if article not in index_items_data_name:   # is article in index_items?
+            print "ARTICLE MISSING", article
+            # ADD Article  to index
+            #insert elements
+            child_li = ET.SubElement(index_ul, 'li')
+            child_li.set('data-name', article )
+            child_li.set('data-touched', articles_dict[article]['touched'])
+            child_li.set('data-section', articles_dict[article]['section'] )
+            child_li.set('data-issue', articles_dict[article]['issue'] )
+            all_categories = articles_dict[article]['topic']
+            all_categories.append( articles_dict[article]['issue'])
+            all_categories.append(articles_dict[article]['section'])
+            all_categories = " ".join(all_categories)
+            child_li.set('class', all_categories )
+            child_li.set('data-categories', all_categories )
+            grandchild_a = ET.SubElement(child_li, 'a')
+            grandchild_a.text = article
+            grandchild_a.set('href', 'html_articles/'+((article.split('/'))[-1])+'.html' )
+
+            #wiki_2_html(article) # CREATE ARTICLE'S TO CONTENT
+
+
+        else: 
             article_pos = index_items_data_name.index(article)
-            if index_items_data_touched[article_pos] != articles_dict[article]['touched']: # check need for update 
-#                print "NOT SAME TOUCHED TIME - UPDATING", article
-                li_found = index_tree.find('.//li[@data-name="{}"]'.format(article))
-                a_found =  index_tree.find('.//a[@data-name="{}"]'.format(article))
-                index_article_set(article, li_found, a_found, articles_dict) # set
-                wiki_2_html(article) # CREATE article's to content file with def wiki_2_html
+            print "ARTICLE PRESENT:", article, "in position:", article_pos
 
-    for article in index_items_data_name: # if category was removed
-        if article not in articles_dict.keys():  
-            li_found = index_tree.find('.//li[@data-name="{}"]'.format(article))
-#            print "MISSING ARTICLE FROM DIC", article, ET.tostring(li_found)
-            index_ul.remove(li_found)
+            # touched time
+            if index_items_data_touched[article_pos] != articles_dict[article]['touched']:
+                print "NOT SAME TOUCHED TIME"
+#                wiki_2_html(article) # UPDATE
 
-#    print ET.tostring(index_tree)
-    write_html_file(ET.tostring(index_tree), 'issue0_index.html')
+                # if yes:    
+                # if is touched date in index.html older that the one from API?
+                # yes:
+                # UPDATE content file & index.html
+                
+                # if not: 
+                # ADD to content file & index.hmlt 
 
+        print article, articles_dict[article]
+        print 
+        print ET.tostring(index_tree)
+        write_html_file(ET.tostring(index_tree), 'index.html')
+
+#         for key in articles_dict[article].keys():
+#             print key, articles_dict[article][key]
+
+
+
+def parse_index(filepath):
+    input_file = open(filepath, 'r') 
+    tree = html5lib.parse(input_file, namespaceHTMLElements=False)
+
+    # find li
+    for ul in tree.findall('.//ul'):        
+        print len(ul), 'ul', ET.tostring(ul), type(ul), ul
+        for li in ul:
+            print 'li', ET.tostring(li)
+
+            # set attributes
+            li_id = li.get('id')
+            li.set('id', li_id+'_x')
+            li.set('class', li_id+'_zz')
+            print li_id
+            
+        #insert elements
+        child = ET.SubElement(ul, 'li')
+        child.text = 'new list item.'
+
+    print ET.tostring(tree)
 
 
 ## Create index
-#index_ul = api_pagesInIssue('Issue0')
+#index_ul = api_pagesInCategory('Issue0')
 #index = template('index', index_ul, index_template)
 #print index
-#write_html_file(index, 'issue0_index.html')
+#write_html_file(index, 'issue0_index.htmlye')
 
+#parse_index('issue0_index.html')
 
-articles_issue_dic = api_pagesInIssue('Issue0')
+issue = 'Issue_0'
+articles_issue_dic = api_pagesInCategory(issue) 
+
 pprint.pprint(articles_issue_dic, width=1)
-edit_index(articles_issue_dic, 'issue0_index.html')
-
-
-wiki_2_html(sys.argv[1]) 
+edit_index(articles_issue_dic, 'index.html')
