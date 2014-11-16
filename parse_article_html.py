31966ac04178286ad5f42a9e9eb5cb357c249b97
Castro0o <andre@andrecastro.info>
2014-11-16 13:19:49 +0100

    working on parse_artcile_html.py to connect media to html page

---
+++ parse_article_html.py
@@ -0,0 +1,129 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#################
+##  imported by mediawiki.py 
+##  MODIFY THE HTML OUTPUT TO BE DISPLAYED IN THE BROWSER
+###########
+from xml.etree import ElementTree as ET
+import urllib2, json, html5lib, sys
+
+
+sid = '1234'
+useragent = "Mozilla/5.001 (windows; U; NT4.0; en-US; rv:1.0) Gecko/25250101"
+endpoint = "http://beyond-social.org/wiki/api.php?format=json&"
+
+
+def api_file_url(filename, api_endpoint): # get full urls
+    filename = filename.replace(' ', '_')
+    query =  "action=query&titles=File:{}&prop=imageinfo&iiprop=url".format(filename)
+    url = api_endpoint + query
+    request = urllib2.urlopen(url)
+    jsonp = json.loads(request.read())    
+    json_dic= ((jsonp.get('query').get('pages')))
+    page_id =  json_dic.keys()[0]
+    page_url = (json_dic.get(page_id).get('imageinfo'))[0].get('url')
+    return page_url 
+
+
+
+def edit_article( article_path ): 
+    htmlfile = open(article_path, 'r') 
+    tree = html5lib.parse(htmlfile, namespaceHTMLElements=False)
+    #print ET.tostring(tree)
+
+    # internal images
+    for img in tree.findall('.//img'): # images
+        src = img.get('src')
+        fileurl = api_file_url(src, endpoint)# find url of file
+        print 'fileurl', fileurl
+        img.set('src', fileurl)
+
+    # WRITE FILE
+    doctype = "<!DOCTYPE html>"
+    html = doctype + ET.tostring(tree,  encoding='utf-8', method='html')
+    edited = open(article_path, 'w') #write
+    print edited
+    edited.write(html)
+    edited.close()
+
+
+'''
+def parsed_html(htmlfile):
+    tree = html5lib.parse(htmlfile, namespaceHTMLElements=False)
+    print ET.tostring(tree)
+    # internal images
+
+    # external images - urls
+
+    # youtube videos
+'''    
+
+def edit_html_media(page, api_endpoint, output_filename): #(pagename)
+    tree = html5lib.parse(page, namespaceHTMLElements=False)
+#    print
+#    print '---- ---- edit_html_media  ---- ----'
+    for img in tree.findall('.//img'): # images
+        src = img.get('src')
+        url = api_file_url(src, api_endpoint)# find url of file
+        img.set('src', url)
+#        print 'IMG', ET.tostring(img)
+#        print
+    for p in tree.findall('.//p'): # youtube       
+        for child in list(p):
+            if child.tag == 'youtube':
+                print '---- ---- youtube  ---- ----'
+                print  ET.tostring(child)
+                print
+                youtube_id = child.text
+                youtube_url = "http://www.youtube.com/embed/{}".format(youtube_id)
+                ET.SubElement(p, 'iframe', 
+                              {"width":"560", "height":"315", "frameborder": "0", "allowfullscreen": "allowfullscreen", "src": youtube_url})
+#                p.append(embed_element)
+#                p.remove(child)
+#    print '---- ---- END: edit_html_media  ---- ----'
+#    print
+    # WRITE FILE
+    doctype = "<!DOCTYPE html>"
+    html = doctype + ET.tostring(tree,  encoding='utf-8', method='html')
+    edited = open('html_articles/' + output_filename, 'w') #write
+#    print edited
+    edited.write(html)
+    edited.close()
+
+
+
+#    tree.write('htmlpage.html', encoding='utf-8', )
+
+#    for figure in tree.findall('.//figure'): #'.//figure/embed/
+#         for element in list(figure):
+#             if element.tag == 'embed':
+#                 src = element.get('src')
+#                 ext = os.path.splitext(src)[1][1:]
+#                 url = api_file_url(src)# find url of file
+#                 caption = figure.find('.//figcaption')
+#                 caption_text = caption.text
+#                 av_tag = '''<div class="{medium}">
+# '<{medium} src="{src}" controls="controls">
+# Sorry, your browser does not support embedded videos. You can download it from <a href="{src}">{src}</a>
+# </{medium}>
+# <figcaption>{caption}</figcaption>
+# </div>'''
+#                 # if video or audio
+#                 if ext in ['ogv', 'mp4', 'webm'] :                    
+#                     av = av_tag.format(src=url, caption=caption_text, medium="video")
+#                 elif ext in ['ogg', 'mp3']:                     
+#                     av = av_tag.format(src=url, caption=caption_text, medium="audio")
+
+#                 av = ET.fromstring(av)
+#                 figure.clear()
+#                 figure.extend(av)
+
+
+
+
+
+line = sys.stdin.read()
+print line
+article = ("articles/{}".format(line) ).replace("\n", "")
+print article
+edit_article(article)
