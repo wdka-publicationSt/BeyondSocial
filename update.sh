9cfb833ac6a5f03b3a9b3dcf84df71b393f370da
Castro0o <andre@andrecastro.info>
2014-11-21 01:19:48 +0100

    update.sh for server

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
+cd /var/www/beyond-social.org/html/beyond-social/
 ./edit_index.py|./create_article.py |./parse_article_html.py
-cp *.html ../ 
-for i in {articles,img,js,styles}; do cp $i ../ -r; done
-
+cp ./*.html ../ 
+for i in {./articles,./img,./js,./styles}; do cp $i ../ -r; done
