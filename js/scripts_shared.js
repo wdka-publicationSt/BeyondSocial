a25df849dc2f9c99c087e225c722292a7e6f6df6
valiz user <contact@template01.info>
2016-06-29 15:50:46 +0200

    from lasse

---
+++ js/scripts_shared.js
@@ -0,0 +1,17 @@
+function shareClick(){
+ 
+  $(document).on('click','#shareButtons a', function(e){
+    e.preventDefault()
+    var currentUrl=window.location
+    if($(this).attr('id') == 'shareTwitter'){
+      window.open('https://twitter.com/intent/tweet?text='+currentUrl); 
+    }else if($(this).attr('id') == 'shareFacebook'){
+      window.open('https://www.facebook.com/sharer/sharer.php?u='+currentUrl); 
+    }
+  })
+
+}
+
+$(document).ready(function(){
+  shareClick()
+})
